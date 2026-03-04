67f6b6a945bd1b9a36cdb2a944c6b9e6a7b278bf
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-03-04 13:48:12 +0200

    feat: remove Fixed subtype

--- src/pixmada-fixed.ads
+++ src/pixmada-fixed.ads
@@ -9,8 +9,6 @@ package Pixmada.Fixed is
      delta 2.0 ** (-32) range -2.0 ** 31 .. 2.0 ** 31 - 2.0 ** (-32);
    for Fixed_32_32'Size use 64;
 
-   subtype Fixed is Fixed_16_16;
-
    Epsilon     : constant Fixed_16_16 := Fixed_16_16'Small;
    One         : constant Fixed_16_16 := 1.0;
    One_Minus_E : constant Fixed_16_16 := One - Epsilon;
